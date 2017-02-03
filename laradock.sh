cc90e33ab53c7632ed0c29728ae01db701dd7b5d
donlinglok <donlinglok@10.10.12.96>
2017-02-03 12:06:58 +0800

    init laravel 5.2

--- laradock.sh
+++ laradock.sh
@@ -11,7 +11,8 @@ sudo chmod +x /usr/bin/docker-compose
 git clone https://github.com/LaraDock/laradock.git
 cp docker-compose.yml laradock/docker-compose.yml
 cd laradock 
-docker-compose build workspace
-docker-compose build nginx
-docker-compose up -d nginx
-      
+docker-compose build workspace & docker-compose build nginx & docker-compose up -d nginx
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+#php composer-setup.php
+#php -r "unlink('composer-setup.php');"
